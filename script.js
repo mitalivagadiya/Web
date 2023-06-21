fd9541e4ae044ffe6be15ecc983a610e21d03389
mitalivagadiya <137184585+mitalivagadiya@users.noreply.github.com>
2023-06-21 10:47:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Example JavaScript code
+console.log("Hello, world!");
+
+// Add an event listener to the header
+var header = document.querySelector('header');
+header.addEventListener('click', function() {
+  alert('You clicked the header!');
+});
