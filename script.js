9122faa647ff04f63e71bbf153a3d7c6a0ab1664
mitalivagadiya <137184585+mitalivagadiya@users.noreply.github.com>
2023-06-22 19:02:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,52 @@
-// Example JavaScript code
-console.log("Hello, world!");
+// Wait for the DOM to finish loading
+document.addEventListener('DOMContentLoaded', function() {
+  // Project click events
+  var projects = document.getElementById('projects');
+  var projectItems = projects.querySelectorAll('li');
+  projectItems.forEach(function(item) {
+    item.addEventListener('click', function() {
+      var projectId = item.getAttribute('data-project-id');
+      if (projectId) {
+        redirectToProjectPage(projectId);
+      }
+    });
+  });
 
-// Add an event listener to the header
-// var header = document.querySelector('header');
-// header.addEventListener('click', function() {
-//  //alert('You clicked the header!');
-//  window.location.href = 'https://www.minopcloud.com/PayTime/EmployeeDashboard';
-// });
+  // Update the contact number
+  var contactNumber = document.getElementById('contactNumber');
+  contactNumber.textContent = '+1 123-456-7890'; // Replace with your actual contact number
 
-// // Get the project list items by their IDs
-//   var project1 = document.getElementById('project1');
-//   var project2 = document.getElementById('project2');
+  // Smooth scroll to section
+  var navLinks = document.querySelectorAll('header nav a');
+  navLinks.forEach(function(link) {
+    link.addEventListener('click', function(event) {
+      event.preventDefault();
+      var targetSection = document.querySelector(link.getAttribute('href'));
+      scrollToSection(targetSection);
+    });
+  });
 
-//   // Add event listeners to each project item
-//   project1.addEventListener('click', function() {
-//     window.location.href = 'https://crccalc.com';
-//   });
+  // Smooth scroll function
+  function scrollToSection(section) {
+    var scrollOptions = {
+      behavior: 'smooth',
+      block: 'start'
+    };
+    section.scrollIntoView(scrollOptions);
+  }
 
-//   project2.addEventListener('click', function() {
-//     window.location.href = 'https://www.fileformat.info/tool/hash.htm';
-//   });
+  // Redirect to project page
+  function redirectToProjectPage(projectId) {
+    // Define the project URLs
+    var projectURLs = {
+      project1: 'https://crccalc.com',
+      project2: 'https://www.fileformat.info/tool/hash.htm',
+      // Add more project URLs here
+    };
+
+    // Check if the project URL exists
+    if (projectURLs.hasOwnProperty(projectId)) {
+      window.location.href = projectURLs[projectId];
+    }
+  }
+});
