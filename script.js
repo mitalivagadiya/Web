7c537859ff470d53f9b16e5fcd2c1c96695fa1f3
mitalivagadiya <137184585+mitalivagadiya@users.noreply.github.com>
2023-06-22 17:13:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ console.log("Hello, world!");
 // Add an event listener to the header
 var header = document.querySelector('header');
 header.addEventListener('click', function() {
- //alert('You clicked the header!');
+ alert('You clicked the header!');
  window.location.href = 'https://www.minopcloud.com/PayTime/EmployeeDashboard';
 });
 
