0ff25aa42c44b6ea357c778719a46a669e53c926
mitalivagadiya <137184585+mitalivagadiya@users.noreply.github.com>
2023-06-22 17:04:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,27 @@
 console.log("Hello, world!");
 
 // Add an event listener to the header
-// var header = document.querySelector('header');
-// header.addEventListener('click', function() {
-//   alert('You clicked the header!');
-// });
+var header = document.querySelector('header');
+header.addEventListener('click', function() {
+  // alert('You clicked the header!');
+  fetch('https://www.minopcloud.com/PayTime/LoginPage')
+  .then(function (response) {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error('Network response was not ok.');
+    }
+  })
+  .then(function (data) {
+    // Handle the data here
+    console.log(data);
+  })
+  .catch(function (error) {
+    // Handle any errors here
+    console.error(error);
+  });
+
+});
 
 fetch('https://www.minopcloud.com/PayTime/LoginPage')
   .then(function (response) {
