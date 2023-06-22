39d4a04151070834ff68532cd71996f917a4f037
mitalivagadiya <137184585+mitalivagadiya@users.noreply.github.com>
2023-06-22 17:06:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,40 +4,6 @@ console.log("Hello, world!");
 // Add an event listener to the header
 var header = document.querySelector('header');
 header.addEventListener('click', function() {
-  // alert('You clicked the header!');
-  fetch('https://www.minopcloud.com/PayTime/LoginPage')
-  .then(function (response) {
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error('Network response was not ok.');
-    }
-  })
-  .then(function (data) {
-    // Handle the data here
-    console.log(data);
-  })
-  .catch(function (error) {
-    // Handle any errors here
-    console.error(error);
-  });
-
+ alert('You clicked the header!');
 });
-
-fetch('https://www.minopcloud.com/PayTime/LoginPage')
-  .then(function (response) {
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error('Network response was not ok.');
-    }
-  })
-  .then(function (data) {
-    // Handle the data here
-    console.log(data);
-  })
-  .catch(function (error) {
-    // Handle any errors here
-    console.error(error);
-  });
 
