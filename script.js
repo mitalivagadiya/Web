837c1c8194052db943d7b8002e26aac48f438956
mitalivagadiya <137184585+mitalivagadiya@users.noreply.github.com>
2023-06-26 11:13:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,6 @@
 // Wait for the DOM to finish loading
-document.addEventListener('DOMContentLoaded', function() {
-  // Project click events
-  var projects = document.getElementById('projects');
-  var projectItems = projects.querySelectorAll('li');
-  projectItems.forEach(function(item) {
-    item.addEventListener('click', function() {
-      var projectId = item.getAttribute('data-project-id');
-      if (projectId) {
-        redirectToProjectPage(projectId);
-      }
-    });
-  });
-
-  // Update the contact number
-  var contactNumber = document.getElementById('contactNumber');
-  contactNumber.textContent = '+1 123-456-7890'; // Replace with your actual contact number
-
-  // Smooth scroll to section
-  var navLinks = document.querySelectorAll('header nav a');
-  navLinks.forEach(function(link) {
-    link.addEventListener('click', function(event) {
-      event.preventDefault();
-      var targetSection = document.querySelector(link.getAttribute('href'));
-      scrollToSection(targetSection);
-    });
-  });
-
+document.addEventListener('DOMContentLoaded', function() 
+{
   // Smooth scroll function
   function scrollToSection(section) {
     var scrollOptions = {
@@ -33,20 +8,5 @@ document.addEventListener('DOMContentLoaded', function() {
       block: 'start'
     };
     section.scrollIntoView(scrollOptions);
-  }
-
-  // Redirect to project page
-  function redirectToProjectPage(projectId) {
-    // Define the project URLs
-    var projectURLs = {
-      project1: 'https://crccalc.com',
-      project2: 'https://www.fileformat.info/tool/hash.htm',
-      // Add more project URLs here
-    };
-
-    // Check if the project URL exists
-    if (projectURLs.hasOwnProperty(projectId)) {
-      window.location.href = projectURLs[projectId];
-    }
   }
 });
